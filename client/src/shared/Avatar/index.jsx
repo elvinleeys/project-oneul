cd25cb42a60d6b77ae00edfc70a66d81dbd9c450
elvinleeys <herjun802@gmail.com>
2026-04-29 20:24:29 +0900

    [Refactor] : 공유 컴포넌트 구성

---
+++ client/src/shared/Avatar/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const Avatar = ({ src, size = 2, alt = "avatar" }) => {
+    return (
+        <Wrapper size={size}>
+            {src ? (
+                <Img src={src} alt={alt} />
+            ) : (
+                <Img
+                    src="/global/images/default.png"
+                    alt="default-profile-image"
+                />
+            )}
+        </Wrapper>
+    );
+};
+
+export default Avatar;
+
+const Wrapper = styled.div`
+    width: ${({ size }) => size}rem;
+    height: ${({ size }) => size}rem;
+    border-radius: 50%;
+    overflow: hidden;
+    flex-shrink: 0;
+    background: #eee;
+`;
+
+const Img = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
