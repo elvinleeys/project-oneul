a7108fab1627a3d065a794c303c3cada9bbe6b88
elvinleeys <herjun802@gmail.com>
2026-04-29 20:28:49 +0900

    [Refactor]: 페이지 재구성에 따른 router 수정

--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -5,12 +5,12 @@ import SignUp from "../pages/signUp/SignUp";
 import Main from "../pages/main/Main";
 import Banner from "../pages/banner/Banner";
 import Search from "../pages/search/Search";
-import OurToday from "../pages/ourToday/OurToday";
+import OurToday from "../pages/ourToday/OurTodayPage";
 import MyMind from "../pages/myMind/MyMind";
 import Calendar from "../pages/calendar/Calendar";
 import MyPage from "../pages/myPage/MyPage";
 import PageNotFound from "../pages/error/PageNotFound";
-import WriteToday from "../pages/ourToday/WriteToday";
+import PostEdit from "../pages/ourToday/PostEditPage";
 import MyPageMain from "../pages/myPage/MyPageMain";
 import BannerMain from "../pages/banner/BannerMain";
 import Banner4 from "../pages/banner/Banner4";
@@ -61,7 +61,7 @@ const router = createBrowserRouter([
             },
             {
                 path: "/writeToday",
-                element: <WriteToday />,
+                element: <PostEdit />,
             },
             {
                 path: "/myMind",
